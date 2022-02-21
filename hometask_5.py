5287263c23bf0d2a14677aa95eb5857eee0641b0
sofiia1998 <Sofiia_Kalishchuk@epam.com>
2022-02-21 23:30:52 +0200

    homework 5 with OOP

--- hometask_5.py
+++ hometask_5.py
@@ -1,81 +1,119 @@
 from datetime import datetime, date
 import calendar
 
-def text_normalization(strName):
-    strSentences = ""
-    words = list(strName.split(". ")) # Create list based on each sentence.
-    for i in range(len(words)): # Loop through list which is each sentence.
-        words[i] = words[i].strip() # Remove any leading or trailing spaces.
-        words[i] = words[i].strip(".") # Remove any periods.
-        words[i] = words[i][:1].upper() + words[i][1:] # Concatenate string with first letter upper.
-        strSentences += words[i] + ".\n" # Concatenate a final string with all sentences.
-    return strSentences
 
-def input_date():
-    year = int(input("Input the expiration date. Year: "))
-    month = int(input("Input the expiration date. Month: "))
-    day = int(input("Input the expiration date. Day: "))
-    exp_date = date(year, month, day)
-    dt_today = datetime.date(datetime.now())
-    time_remaining = exp_date - dt_today
-    time_remaining1 = time_remaining.days
-    print(time_remaining1)
-    return time_remaining1, exp_date
+class Normalization:
+    def __init__(self, str_name):
+        self.str_name = str_name
 
-def add_to_file(text):
-    try:
-        with open("newsfeed.txt", "x") as f:
-            f.write(text)
-            f.close()
-    except:
-        f = open(r"newsfeed.txt", "a")
-        f.write(text)
-        f.close()
+    def text_normalization(self):
+        self.str_sentences = ""
+        words = list(self.str_name.split("."))  # Create list based on each sentence.
+        for i in range(len(words)-1):  # Loop through list which is each sentence.
+            words[i] = words[i].strip()  # Remove any leading or trailing spaces.
+            words[i] = words[i].strip(".")  # Remove any periods.
+            words[i] = words[i][:1].upper() + words[i][1:]  # Concatenate string with first letter upper.
+            self.str_sentences += words[i] + ".\n"  # Concatenate a final string with all sentences.
+        return self.str_sentences
 
-a = """Hello!
+
+class Inputting:
+    def __init__(self, blck="""Hello!
 Choose 1 to add item to the \"News\"
 Choose 2 to add item to the \"Private ad\"
 Choose 3 to add item to the \"Word of the day\"
 Choose 4 to exit
-Your choice: """
-a1 = """1 to add item to the \"News\"
+Your choice: """, blck1="""1 to add item to the \"News\"
 2 to add item to the \"Private ad\"
 3 to add item to the \"Word of the day\"
 4 to exit
-Your choice: """
+Your choice: """):
+        self.blck = blck
+        self.blck1 = blck1
 
-def text_input(text):
-    global user_choice
-    user_choice = int(input(text))
-    print(f"Your choice is {user_choice}")
+    def text_input(self, text):
+        global user_choice
+        user_choice = int(input(text))
+        print(f"Your choice is {user_choice}")
+        return user_choice
 
-try:
-    text_input(a)
-except:
-    print("Please, type only 1, 2, 3, or 4")
-    text_input(a1)
+    def error_handling_for_input(self):
+        try:
+            self.text_input(self.blck)
+        except:
+            print("Please, type only 1, 2, 3, or 4")
+            self.text_input(self.blck1)
 
-while user_choice:
-    if user_choice == 1:
-        news_text = input("Write the news: ")
-        city = input("The origin of the news (City): ")
-        dt_string = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        a = text_normalization(news_text)
-        add_to_file(f"News -------------------------\n{a}{city}, {dt_string}\n------------------------------\n\n")
-        text_input(a1)
-    elif user_choice == 2:
-        ad_text = input("Write the private advertisement: ")
-        a = text_normalization(ad_text)
-        d, dt = input_date()
-        add_to_file(f"Private Ad -------------------\n{a}Actual until: {dt}, {d} days left\n------------------------------\n\n")
-        text_input(a1)
-    elif user_choice == 3:
-        word = input("Write a word of the day: ")
-        a = text_normalization(word)
-        curr_date = date.today()
-        weekd = calendar.day_name[curr_date.weekday()]
-        add_to_file(f"A word of the day: -----------\n{a}Today is {weekd}\n------------------------------\n\n")
-        text_input(a1)
-    else:
-        user_choice = False
-print("The end!")
+    def input_date(self):
+        self.year = int(input("Input the expiration date. Year: "))
+        self.month = int(input("Input the expiration date. Month: "))
+        self.day = int(input("Input the expiration date. Day: "))
+        self.exp_date = date(self.year, self.month, self.day)
+        self.dt_today = datetime.date(datetime.now())
+        self.time_remaining = self.exp_date - self.dt_today
+        self.time_remaining1 = self.time_remaining.days
+        return self.time_remaining1, self.exp_date
+
+
+class AddToFile:
+    def add_to_file(self, text):
+        try:
+            with open("newsfeed.txt", "x") as f:
+                f.write(text)
+                f.close()
+        except:
+            f = open(r"newsfeed.txt", "a")
+            f.write(text)
+            f.close()
+
+
+class Operational:
+    def __init__(self, a="""Choose 1 to add item to the \"News\"
+Choose 2 to add item to the \"Private ad\"
+Choose 3 to add item to the \"Word of the day\"
+Choose 4 to exit
+Your choice: """):
+        self.a = a
+
+    def while_loop(self):
+        global user_choice
+        input1 = Inputting()
+        input1.error_handling_for_input()
+        while user_choice:
+            if user_choice == 1:
+                news_text = input("Write the news: ")
+                city = input("The origin of the news (City): ")
+                dt_string = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+                norm1 = Normalization(news_text)
+                norm = norm1.text_normalization()
+                text1 = AddToFile()
+                text1.add_to_file(f"News -------------------------\n{norm}{city}, {dt_string}\n------------------------------\n\n")
+                input1.text_input(self.a)
+            elif user_choice == 2:
+                ad_text = input("Write the private advertisement: ")
+                norm1 = Normalization(ad_text)
+                norm = norm1.text_normalization()
+                d, dt = input1.input_date()
+                text1 = AddToFile()
+                text1.add_to_file(f"Private Ad -------------------\n{norm}Actual until: {dt}, {d} days left\n------------------------------\n\n")
+                input1.text_input(self.a)
+            elif user_choice == 3:
+                word = input("Write a word of the day: ")
+                norm1 = Normalization(word)
+                norm = norm1.text_normalization()
+                curr_date = date.today()
+                weekd = calendar.day_name[curr_date.weekday()]
+                text1 = AddToFile()
+                text1.add_to_file(f"A word of the day: -----------\n{norm}Today is {weekd}\n------------------------------\n\n")
+                input1.text_input(self.a)
+            elif user_choice == 4:
+                print("The end!")
+                user_choice = False
+            else:
+                pass
+                print("Please, type only 1, 2, 3, or 4")
+                input1.text_input(self.a)
+
+if __name__ == "__main__":
+    x = Operational()
+    x.while_loop()
