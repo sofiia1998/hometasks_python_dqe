79e0cf9a69bc613acfaf9a3a170b02c2cfe799b7
sofiia1998 <Sofiia_Kalishchuk@epam.com>
2022-02-23 01:16:17 +0200

    homework 5 final

--- hometask_5.py
+++ hometask_5.py
@@ -17,35 +17,32 @@ class Normalization:
             words[i] = words[i][:1].upper() + words[i][1:]  # Concatenate string with first letter upper.
             self.str_sentences += words[i] + ".\n"  # Concatenate a final string with all sentences.
         return self.str_sentences
+
 # a = Normalization("Some text for the check. one more sentence.the last one.")
 # print(a.text_normalization())
 
+
 class Inputting:
-    def __init__(self, blck="""Hello!
-Choose 1 to add item to the \"News\"
-Choose 2 to add item to the \"Private ad\"
-Choose 3 to add item to the \"Word of the day\"
-Choose 4 to exit
-Your choice: """, blck1="""1 to add item to the \"News\"
+    def __init__(self, blck1="""Choose: 
+1 to add item to the \"News\"
 2 to add item to the \"Private ad\"
 3 to add item to the \"Word of the day\"
 4 to exit
-Your choice: """):
-        self.blck = blck
+Your choice: """, count=True, user_choice=True):
         self.blck1 = blck1
-
-    def text_input(self, text):
-        global user_choice
-        user_choice = int(input(text))
-        print(f"Your choice is {user_choice}")
-        return user_choice
+        self.count = count
+        self.user_choice = user_choice
 
     def error_handling_for_input(self):
-        try:
-            self.text_input(self.blck)
-        except:
-            print("Please, type only 1, 2, 3, or 4")
-            self.text_input(self.blck1)
+        while self.count:
+            try:
+                self.user_choice = int(input(self.blck1))
+                print(f"Your choice is {self.user_choice}")
+                while self.user_choice == 0:
+                    self.user_choice = int(input(self.blck1))
+                return self.user_choice
+            except:
+                print("Please, type only 1, 2, 3, or 4")
 
     def input_date(self):
         self.year = int(input("Input the expiration date. Year: "))
@@ -71,19 +68,12 @@ class AddToFile:
 
 
 class Operational:
-    def __init__(self, a="""Choose 1 to add item to the \"News\"
-Choose 2 to add item to the \"Private ad\"
-Choose 3 to add item to the \"Word of the day\"
-Choose 4 to exit
-Your choice: """):
-        self.a = a
 
     def while_loop(self):
-        global user_choice
         input1 = Inputting()
         input1.error_handling_for_input()
-        while user_choice:
-            if user_choice == 1:
+        while input1.user_choice:
+            if input1.user_choice == 1:
                 news_text = input("Write the news: ")
                 city = input("The origin of the news (City): ")
                 dt_string = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
@@ -91,16 +81,16 @@ Your choice: """):
                 norm = norm1.text_normalization()
                 text1 = AddToFile()
                 text1.add_to_file(f"News -------------------------\n{norm}{city.title()}, {dt_string}\n------------------------------\n\n")
-                input1.text_input(self.a)
-            elif user_choice == 2:
+                input1.error_handling_for_input()
+            elif input1.user_choice == 2:
                 ad_text = input("Write the private advertisement: ")
                 norm1 = Normalization(ad_text)
                 norm = norm1.text_normalization()
                 d, dt = input1.input_date()
                 text1 = AddToFile()
                 text1.add_to_file(f"Private Ad -------------------\n{norm}Actual until: {dt}, {d} days left\n------------------------------\n\n")
-                input1.text_input(self.a)
-            elif user_choice == 3:
+                input1.error_handling_for_input()
+            elif input1.user_choice == 3:
                 word = input("Write a word of the day: ")
                 norm1 = Normalization(word)
                 norm = norm1.text_normalization()
@@ -108,14 +98,14 @@ Your choice: """):
                 weekd = calendar.day_name[curr_date.weekday()]
                 text1 = AddToFile()
                 text1.add_to_file(f"A word of the day: -----------\n{norm}Today is {weekd}\n------------------------------\n\n")
-                input1.text_input(self.a)
-            elif user_choice == 4:
+                input1.error_handling_for_input()
+            elif input1.user_choice == 4:
                 print("The end!")
-                user_choice = False
+                break
             else:
-                pass
                 print("Please, type only 1, 2, 3, or 4")
-                input1.text_input(self.a)
+                input1.error_handling_for_input()
+
 
 if __name__ == "__main__":
     x = Operational()
