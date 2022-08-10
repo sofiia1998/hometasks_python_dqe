d3e5c24dd1ccdf7571d1704c80b2af40858c1a7f
sofiia1998 <Sofiia_Kalishchuk@epam.com>
2022-08-10 15:17:38 +0200

    homework 6 final

--- hometask_5.py
+++ hometask_5.py
@@ -1,22 +1,30 @@
 from datetime import datetime, date
 import calendar
+import os
+import re
 
 
 class Normalization:
-    def __init__(self, str_name):
+    def __init__(self, str_name, str_name1):
         self.str_name = str_name
+        self.str_name1 = str_name1
 
     def text_normalization(self):
-        self.str_sentences = ""
-        if (self.str_name[-1] == '.'): # remove the last period to avoid double periods in the last sentence
-            self.str_name = self.str_name[:-1]
-        words = list(self.str_name.split("."))  # Create list based on each sentence.
-        for i in range(len(words)):  # Loop through list which is each sentence.
-            words[i] = words[i].strip()  # Remove any leading or trailing spaces.
-            words[i] = words[i].strip(".")  # Remove any periods.
-            words[i] = words[i][:1].upper() + words[i][1:]  # Concatenate string with first letter upper.
-            self.str_sentences += words[i] + ".\n"  # Concatenate a final string with all sentences.
-        return self.str_sentences
+        try:
+            self.str_sentences = ""
+            if (self.str_name[-1] == '.'): # remove the last period to avoid double periods in the last sentence
+                self.str_name = self.str_name[:-1]
+            sentences = list(self.str_name.split("."))  # Create list based on each sentence.
+            for i in range(len(sentences)):  # Loop through list which is each sentence.
+                sentences[i] = sentences[i].strip()  # Remove any leading or trailing spaces.
+                sentences[i] = sentences[i].strip(".")  # Remove any periods.
+                sentences[i] = sentences[i][:1].upper() + sentences[i][1:]  # Concatenate string with first letter upper.
+                self.str_sentences += sentences[i] + ".\n"  # Concatenate a final string with all sentences.
+            return self.str_sentences
+        except:
+            print('hello?')
+            text2 = AddToFile()
+            text2.add_to_file_false(self.str_name1)
 
 # a = Normalization("Some text for the check. one more sentence.the last one.")
 # print(a.text_normalization())
@@ -54,6 +62,13 @@ Your choice: """, count=True, user_choice=True):
         self.time_remaining1 = self.time_remaining.days
         return self.time_remaining1, self.exp_date
 
+    def input_date1(self, year, month, day):
+        self.exp_date = date(year, month, day)
+        self.dt_today = datetime.date(datetime.now())
+        self.time_remaining = self.exp_date - self.dt_today
+        self.time_remaining1 = self.time_remaining.days
+        return self.time_remaining1, self.exp_date
+
 
 class AddToFile:
     def add_to_file(self, text):
@@ -63,6 +78,15 @@ class AddToFile:
                 f.close()
         except:
             f = open(r"newsfeed.txt", "a")
+            f.write(text)
+            f.close()
+    def add_to_file_false(self, text):
+        try:
+            with open("newsfeed_false.txt", "x") as f:
+                f.write(text)
+                f.close()
+        except:
+            f = open(r"newsfeed_false.txt", "a")
             f.write(text)
             f.close()
 
@@ -80,7 +104,7 @@ class Operational:
                 norm1 = Normalization(news_text)
                 norm = norm1.text_normalization()
                 text1 = AddToFile()
-                text1.add_to_file(f"News -------------------------\n{norm}{city.title()}, {dt_string}\n------------------------------\n\n")
+                text1.add_to_file(f"News -------------------------\n{norm}{city.title()},{dt_string}\n------------------------------\n\n")
                 input1.error_handling_for_input()
             elif input1.user_choice == 2:
                 ad_text = input("Write the private advertisement: ")
@@ -88,7 +112,7 @@ class Operational:
                 norm = norm1.text_normalization()
                 d, dt = input1.input_date()
                 text1 = AddToFile()
-                text1.add_to_file(f"Private Ad -------------------\n{norm}Actual until: {dt}, {d} days left\n------------------------------\n\n")
+                text1.add_to_file(f"Private Ad -------------------\n{norm}Actual until: {d3t}, {d} days left\n------------------------------\n\n")
                 input1.error_handling_for_input()
             elif input1.user_choice == 3:
                 word = input("Write a word of the day: ")
@@ -107,6 +131,83 @@ class Operational:
                 input1.error_handling_for_input()
 
 
-if __name__ == "__main__":
-    x = Operational()
-    x.while_loop()
+class Operational1:
+
+    def main_code(self):
+        with open(r'C:\Users\Sofiia_Kalishchuk\hometasks_python_dqe\data') as f:
+            contents = f.readlines()
+            print(contents)
+        contents1 = []
+
+        for i in range(len(contents)):
+            contents1.append(contents[i].strip("\n"))
+        print(contents1)
+
+        listy1 = []
+        for i in range(len(contents1)):
+            listy1.append(contents1[i].split("/"))
+            listy1[i] = list(filter(None, listy1[i]))
+        print(listy1)
+
+        listy = ['NEWS', 'PRIVAT_AD', 'WORD_OF_THE_DAY']
+
+        for i in range(len(listy1)):
+            if listy1[i][0] == listy[0]:
+                print('it is NEWS line')
+                len_attr = len(listy1[i])
+                if len_attr == 3:
+                    norm1 = Normalization(listy1[i][1], listy1[i])
+                    norm = norm1.text_normalization()
+                    dt_string = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+                    text1 = AddToFile()
+                    text1.add_to_file(
+                        f"News -------------------------\n{norm}{listy1[i][2].title()}, {dt_string}\n------------------------------\n\n")
+                else:
+                    text2 = AddToFile()
+                    text2.add_to_file_false(str(listy1[i]) + "\n")
+            elif listy1[i][0] == listy[1]:
+                print('it is ADs line')
+                len_attr = len(listy1[i])
+                if len_attr == 5:
+                    norm1 = Normalization(listy1[i][1], listy1[i])
+                    norm = norm1.text_normalization()
+                    matched1 = re.match('[2][0][2-9][2-9]', (listy1[i][2]))
+                    matched2 = re.match('[1-12]', (listy1[i][3]))
+                    matched3 = re.match('[1-31]', (listy1[i][4]))
+                    is_match1 = bool(matched1)
+                    is_match2 = bool(matched2)
+                    is_match3 = bool(matched3)
+                    if is_match1 == True and is_match2 == True and is_match3 == True:
+                        input1 = Inputting()
+                        d, dt = input1.input_date1(int(listy1[i][2]), int(listy1[i][3]), int(listy1[i][4]))
+                        text1 = AddToFile()
+                        text1.add_to_file(
+                            f"Private Ad -------------------\n{norm}Actual until: {dt}, {d} days left\n------------------------------\n\n")
+                    else:
+                        text2 = AddToFile()
+                        text2.add_to_file_false(str(listy1[i]) + "\n")
+                else:
+                    text2 = AddToFile()
+                    text2.add_to_file_false(str(listy1[i]) + "\n")
+            else:
+                print('it is WORD OF THE DAY line')
+                len_attr = len(listy1[i])
+                listy[i].strip('')
+                if len_attr == 2:
+                    norm1 = Normalization(listy1[i][1], listy1[i])
+                    norm = norm1.text_normalization()
+                    curr_date = date.today()
+                    weekd = calendar.day_name[curr_date.weekday()]
+                    text1 = AddToFile()
+                    text1.add_to_file(
+                        f"A word of the day: -----------\n{norm}Today is {weekd}\n------------------------------\n\n")
+                else:
+                    text2 = AddToFile()
+                    text2.add_to_file_false(str(listy1[i]) + "\n")
+
+        #os.remove(r'C:\Users\Sofiia_Kalishchuk\hometasks_python_dqe\data')
+
+
+# if __name__ == "__main__":
+#     x = Operational()
+#     x.while_loop()
---
+++ hometask_6.py
@@ -0,0 +1,8 @@
+from hometask_5 import Operational1
+
+
+
+x = Operational1()
+x.main_code()
+
+
